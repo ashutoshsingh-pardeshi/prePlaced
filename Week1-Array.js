f7d31d6a7f49e48e38c072b487a54daf3309ec70
Ashutosh Pardeshi <ashutoshpardeshi11@gmail.com>
2025-04-07 09:29:08 +0530

    add: Min-Max function

--- Week1-Array.js
+++ Week1-Array.js
@@ -2,7 +2,7 @@
 // Link: https://www.geeksforgeeks.org/problems/peak-element/1
 
 let array1 = [1, 2, 4, 5, 7, 8, 3];
-console.log("# Peak exist :", peakElement(array1));
+// console.log("# Peak exist :", peakElement(array1));
 
 function peakElement(arr) {
   // If array id empty
@@ -33,3 +33,29 @@ function peakElement(arr) {
 
 // Program to find the minimum (or maximum) element of an array
 // Link: https://www.geeksforgeeks.org/program-find-minimum-maximum-element-array/
+
+console.log("Minimum element = ", findMinMax(array1, 1));
+console.log("Maximum element = ", findMinMax(array1, 0));
+function findMinMax(arr, isMin) {
+  // isMin undefined or out of boundry
+  if (isMin === undefined || ![1, 0].includes(isMin)) return -1;
+
+  // Empty array
+  if (arr.length === 0) return -1;
+
+  // Single element array
+  if (arr.length === 1) return arr[0];
+
+  // Finding the minmum by looping
+  let num = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (isMin === 1 && num > arr[i])
+      // Find minimum
+      num = arr[i];
+    else if (isMin === 0 && num < arr[i])
+      // Find maximum
+      num = arr[i];
+  }
+
+  return num;
+}
